f2b8352fde673be43a46bd4e0fa00f984219675b
FranciscoKilibarda <panchitokili555@gmail.com>
2025-11-19 19:59:05 -0400

    Coloque nuevos archivoas a las carpeta

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const questionsRoutes = require('./routes/questions.routes');
+const examsRoutes = require('./routes/exams.routes');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middlewares
 app.use(cors());
-app.use(express.json()); // Para que entienda los datos del formulario
+app.use(express.json());
 
 // Ruta de prueba
 app.get('/', (req, res) => {
   res.send('API de Encuestas - Funcionando 🚀');
 });
+
+// Rutas del API
+app.use('/api/questions', questionsRoutes);
+app.use('/api/exams', examsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
