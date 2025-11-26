8fc00fc7c8f00e853756825a2e204427ace15c47
FranciscoKilibarda <panchitokili555@gmail.com>
2025-11-26 00:18:01 -0400

    Ahora si recien se conecto la base de datos xd antes solo se creava la ruta mas no conectava

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
+// src/index.js
 const express = require('express');
 const cors = require('cors');
+
 require('dotenv').config();
+console.log('DEBUG desde index.js, MONGO_URI =', process.env.MONGO_URI);
+
+const connectMongo = require('./config/mongo'); // 👈 SOLO UNA VEZ
+
+console.log('>>> index.js se está ejecutando');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors());
-app.use(express.json()); // Para que entienda los datos del formulario
+app.use(express.json());
 
 // Ruta de prueba
 app.get('/', (req, res) => {
-  res.send('API de Encuestas - Funcionando ');
+  res.send('API de Encuestas con MongoDB - Funcionando 🚀');
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    console.log('>>> llamando a connectMongo');
+    await connectMongo(); // conecta a MongoDB
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error('❌ Error al iniciar el servidor:', err.message);
+  }
+};
+
+startServer();
--- src/models/dificultad.model.js
+++ src/models/dificultad.model.js
@@ -5,15 +5,15 @@ require('dotenv').config();
 const connectMongo = async () => {
   try {
     const uri = process.env.MONGO_URI;
-    console.log('>>> connectMongo llamado');                // DEBUG
-    console.log('MONGO_URI que estoy usando:', uri);        // DEBUG
+    console.log('>>> connectMongo llamado');
+    console.log('MONGO_URI que estoy usando:', uri);
 
     console.log(' Conectando a MongoDB...');
     await mongoose.connect(uri);
     console.log(' MongoDB conectado correctamente ✅');
   } catch (error) {
     console.error('❌ Error conectando a MongoDB:', error.message);
-    throw error; // dejamos que index.js lo capture
+    throw error;
   }
 };
 
