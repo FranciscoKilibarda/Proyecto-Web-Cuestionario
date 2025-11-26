8933066694ee5c33f74e69f8be55380a3ebf6823
FranciscoKilibarda <panchitokili555@gmail.com>
2025-11-26 17:24:31 -0400

    Borre archivos que estavan de mas en la carpeta routers

--- src/routes/exams.routes.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const ExamController = require('../controllers/exam.controller');
-
-router.get('/', ExamController.getAll);
-
-module.exports = router;
